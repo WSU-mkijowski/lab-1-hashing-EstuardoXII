fcc88c24e923fe560d784340bb2c4c43e6c0cf9b
EstuardoXII <estuardoxii@Outlook.com>
2025-09-04 17:54:24 -0400

    Task Two Bonus Modified tasktwo.sh script to generate a 5 leading 0's coin Added new coin to coins.txt

--- miner/tasktwo.sh
+++ miner/tasktwo.sh
@@ -2,22 +2,22 @@
 
 dictionary="../data/dictionary"
 coins_file="../coins.txt"
-required_zeros="000"
+required_zeros="000000"
 
 while IFS= read -r word; do
-  for ((i=0; i<=9999; i++)); do
-    # make nonces 4 digits
-    salt=$(printf "%04d" "$i")
+  for ((i=0; i<=999999; i++)); do
+    # generate a 6 digit nonce
+    salt=$(printf "%06d" "$i")
 
-    # combine salt and word
-    salted_coin="$salt""$word"
+    # combine nonce and word
+    nonce_coin="$nonce""$word"
 
     # perform a sha256sum on salt+word
-    hash_coin=$(printf "%s" $salted_coin | sha256sum | awk '{print $1}')
+    hash_coin=$(printf "%s" $nonce_coin | sha256sum | awk '{print $1}')
 
     # check if the hash of the potential coin starts with the required zeros, if yes append to coins.txt
     if [[ $hash_coin == ${required_zeros}* ]]; then
-      printf "%s\n" "$salted_coin" >> "$coins_file"
+      printf "%s\n" "$nonce_coin" >> "$coins_file"
     fi
 
   done
