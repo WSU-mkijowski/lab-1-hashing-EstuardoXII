20472b3124a9fecd1ff0043504144461b0e8fa7c
EstuardoXII <estuardoxii@Outlook.com>
2025-09-04 14:24:23 -0400

    Task One Created a script to generate a salt plut salted hash and replace the corresponding message. The changes were saved to the file name salted_data.csv

---
+++ taskone.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# make an array of the usernames excluding the header, and removing duplicates
+list=($(tail -n +2 data/quiz_data.csv | awk -F ',' '{ print $1 }' | sort -u))
+
+for i in "${!list[@]}"; do
+  # generate a random 5 digit salt
+  salt=$(printf "%05d" $((RANDOM % 100000)))
+
+  # append the salt to the username and perform a sha256sum
+  hash=$(printf "${salt}${list[$i]}" | sha256sum | awk '{print $1}')
+
+  salt_hash="$salt""$hash"
+
+  # replace the username with the salt + hash
+  sed -i "s/${list[$i]}/$salt_hash/" salted_data.csv
+done
